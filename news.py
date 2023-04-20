b78ad1ae2ce09216f5b2b56631a3e33574c62250
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2023-04-20 18:46:28 +0530

    added working getNews function

--- news.py
+++ news.py
@@ -1,29 +1,35 @@
+# returns news in a json file, in this format, under "articles"
+# 
+# {
+#     "author" : "...",
+#     "title" : "...",
+#     "description" : "...",
+#     "url" : "...",
+#     "urlToImage" : "...",
+#     "publishedAt" : "...",
+# }
+
 import json
 import requests 
 from urllib.request import urlopen
 import voiceParser as vp
 
+def getNews():
 
-queryParams = {
-    "source" : "bbc-news",
-    "sortBy" : "top",
-    "apiKey" : "a43bbf8815e84611860412499d7710ac"
-}
+    queryParams = {
+        "source" : "bbc-news",
+        "sortBy" : "top",
+        "apiKey" : "a43bbf8815e84611860412499d7710ac"
+    }
 
-mainUrl = "https://newsapi.org/v1/articles"
+    mainUrl = "https://newsapi.org/v1/articles"
 
-# fetching data in a json file
-res = requests.get(mainUrl, params = queryParams)
+    # fetching data in a json file
+    res = requests.get(mainUrl, params = queryParams)
 
-data = res.json()
-i = 1
-             
-vp.speak('here is some top news from BBC news')
-print('''=============== BBC ============'''+ '\n')
-                
-for item in data['articles']:
-    print(str(i) + '. ' + item['title'] + '\n')
-    print(item['description'] + '\n')
-    vp.speak(str(i) + '. ' + item['title'] + '\n')
-    i += 1                    
-                    
+    data = res.json()
+
+    return data
+
+           
+      
