e61b93802af5b1966c8c0d25b3ad80175f98f5ba
ManselInfinity <106764539+ManselInfinity@users.noreply.github.com>
2023-04-20 21:45:39 +0530

    minor changes

--- newsAlgorithm.py
+++ newsAlgorithm.py
@@ -1,15 +1,18 @@
 # module to find what news the user wants and suggest news accordingly 
+# input the description of the news to the recommendedNews() function 
 
 from nltk.tokenize import word_tokenize 
 from nltk.probability import FreqDist
 from nltk.corpus import stopwords
 
-
 import json
 
+
 def recommendedNews(news):
+   
     f = open('database.json', "+r")
     data = json.load(f)
+    
     wordTokens = word_tokenize(news)
 
     # removing stopwords(words that are not tokens, like "the", "a", "is"...)
@@ -27,11 +30,12 @@ def recommendedNews(news):
     keyWords = FreqDist(filteredTokens)
     tokens = keyWords.most_common(10)
 
-    print(tokens)
+    #print(tokens)
     
-    # for item in data["userNewsLikings"] :
-    #     for keyWord in tokens :
-    #         if(keyWord == item):
+    for item in data["userNewsLikings"] :
+       for key, value in tokens :
+             if(value == item):
+                
 
 
 
